e6a32aa7b3971424a7936ba290c420b41fe90788
Maxim Baulin <mvbaulin@yandex.ru>
2024-06-17 21:07:56 +0300

    Temp

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -12,7 +12,7 @@ const ProductItem = ({product, className, onAdd}) => {
       <div className="img" />
       <div className="title">{product.title}</div>
       <div className="description">{product.description}</div>
-      <div className="price">{product.price}
+      <div className="price">
         <span>Стоимость: <b>{product.price}</b></span>
       </div>
       <Button className='add-btn' onClick={onAddHandler}>
